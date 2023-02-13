563e5d64472816c7254c90a36bd347e427ef0d1c
rajukare <bangarrajuk23@gmail.com>
2023-02-13 17:27:23 +0530

    third commit

--- index.js
+++ index.js
@@ -14,7 +14,12 @@
 // console.log(sub(6,3));
 // console.log(add(4,6));
 
-const path = require('path');
+// const path = require('path');
 
-const filePath = path.join("users","files","inf.txt");
-console.log(filePath);
+// const filePath = path.join("users","files","inf.txt");
+// console.log(filePath);
+
+const addDays = require('date-fns/addDays');
+
+const result = addDays(new Date(2019,0,1), 3);
+console.log(result);
